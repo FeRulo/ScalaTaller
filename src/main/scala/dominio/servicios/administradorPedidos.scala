284335c1985467cd9569accdd85351af84f8bf36
josepaez <josepaez@seven4n.com>
2018-04-24 17:23:03 -0500

    mejoramiento de diseño para no recibir efectos en paralelo

--- src/main/scala/dominio/servicios/administradorPedidos.scala
+++ src/main/scala/dominio/servicios/administradorPedidos.scala
@@ -8,38 +8,46 @@ import scala.util.Try
 import util.pool.global
 
 sealed trait AdministradorPedidos{
-  def reportar(pedidos:List[Pedido]): List[String]
+  def reportar(pedidos:List[Pedido]): Future[List[String]]
+}
+object tiempo{
+  var espera = 1100
 }
 
 sealed trait InterpreteAdministradorPedidos extends AdministradorPedidos{
 
-  override def reportar(pedidos:List[Pedido]): List[String] = {
-    val fl = Future
-        .sequence(
-          pedidos
-            .map(lp=>Future{lp})
-            .map(f=>f
-              .map(p=>{
-                Thread.sleep(500)
-                print(s"trabajando dentro de hilo: ${Thread.currentThread().getName}\n")
-                InterpreteServicioDron.reportarRutas(Try{p.rutas},p.dron,Limite(10)).get
-              })
-            )
-            .zip(inicializarArchivosSalida)
-            .map(tu=>tu._1.map(s=>
-              escribirReporteEnArchivo(tu._2,s)
-            ))
+  override def reportar(pedidos:List[Pedido]): Future[List[String]] = {
+    Future
+      .sequence(
+        pedidos.map(lp=>Future{lp})
+        .map(f=>f.flatMap(p=>reportarUn(p)))
+        .zip(inicializarArchivosSalida)
+        .map(tu=>tu._1.map(s=>
+            escribirReporteEnArchivo(tu._2,s)
+          )
         )
-    Await.result(fl, 10 seconds)
+      )
   }
 
-  def inicializarPedidos(): List[Pedido] = {
-    inicializarArchivosEntrada
-      .map(s=>InterpreteServicioArchivo.leerArchivo(s))
-      .zip(inicializarFlota)
-      .filter(tu=>tu._1.isSuccess)
-      .map(tu=>(tu._1.get,tu._2))
-      .map(tu=>Pedido(tu._1,tu._2))
+  def reportarUn(pedido:Pedido):Future[String] ={
+    Thread.sleep(tiempo.espera)
+    Future{InterpreteServicioDron.reportarRutas(Try{pedido.rutas},pedido.dron,Limite(10)).get}
+  }
+
+  def inicializarPedidos(listaArchivos: List[String]): Future[List[Pedido]] = {
+    Future.sequence(listaArchivos
+      .map(s=>Future{s})
+      .map(f=>f
+        .map(s=>{
+          Thread.sleep(tiempo.espera)
+          InterpreteServicioArchivo.leerArchivo(s)
+        })
+      )).map(l=>l
+          .zip(inicializarFlota)
+          .filter(tu=>tu._1.isSuccess)
+          .map(tu=>(tu._1.get,tu._2))
+          .map(tu=>Pedido(tu._1,tu._2))
+        )
   }
   def inicializarArchivosEntrada(): List[String] = {
     (1 to 20).map(i=>f"src/main/resources2/in/in$i%02d.txt").toList
--- src/test/scala/testArchivos/MultiplesDronesSuite.scala
+++ src/test/scala/testArchivos/MultiplesDronesSuite.scala
@@ -2,40 +2,53 @@ package testArchivos
 
 import java.io.FileNotFoundException
 
-import dominio.entidades._
-import dominio.servicios.InterpreteServicioArchivo._
-import dominio.servicios.InterpreteServicioDron._
-import dominio.servicios.{InterpreteAdministradorPedidos, InterpreteServicioRuta}
+import dominio.servicios.{InterpreteAdministradorPedidos, InterpreteServicioRuta, tiempo}
 import org.scalatest.FunSuite
 import util.pool.global
 
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.util.Success
 
 class MultiplesDronesSuite extends FunSuite{
   object servicioRuta extends InterpreteServicioRuta
 
-  test("Executor Context propio desde packaje object"){
-    val f = Future{
-      println(Thread.currentThread().getName)
-      1
-    }
-  }
-
-  test("probando inicializar pedidos"){
-    val pedidos = InterpreteAdministradorPedidos.inicializarPedidos()
+ test("probando inicializar pedidos"){
+    val listaArchivos = InterpreteAdministradorPedidos.inicializarArchivosEntrada()
+    val pedidos = InterpreteAdministradorPedidos.inicializarPedidos(listaArchivos)
+    val fl = pedidos.flatMap(lp=>InterpreteAdministradorPedidos.reportar(lp))
     assertResult((1 to 20).map(i=>"Escritura Exitosa").toList){
-      InterpreteAdministradorPedidos.reportar(pedidos)
+      Await.result(fl, 10 seconds)
     }
   }
-  test("probar tiempo en paralelo"){
 
-    val estimatedElapsed = (500+50)/1000
+  test("probar tiempo de inicializar Pedidos en paralelo"){
+    val listaArchivos = InterpreteAdministradorPedidos.inicializarArchivosEntrada()
+    val estimatedElapsed:Double =  (tiempo.espera+10D)/1000
+
     val t1 = System.nanoTime()
-    val pedidos = InterpreteAdministradorPedidos.inicializarPedidos()
-    InterpreteAdministradorPedidos.reportar(pedidos)
+    val pedidos = InterpreteAdministradorPedidos.inicializarPedidos(listaArchivos)
+    Await.result(pedidos, 10 seconds)
     val elapsed = (System.nanoTime() - t1) / 1.0E09
-    println(s"futuros iniciador fuera del for-comp: estimado: $estimatedElapsed ,real: $elapsed")
+
+    println(s"tiempo de espera: estimado: $estimatedElapsed ,real: $elapsed")
+    assert(elapsed >= estimatedElapsed)
+  }
+
+  test("probar tiempo de reportar en paralelo"){
+    val listaArchivos = InterpreteAdministradorPedidos.inicializarArchivosEntrada()
+    val pedidos = InterpreteAdministradorPedidos.inicializarPedidos(listaArchivos)
+
+    val estimatedElapsed:Double =  (tiempo.espera+10D)/1000
+
+    val t1 = System.nanoTime()
+    val fl = for{
+      lp<-pedidos
+    }yield(InterpreteAdministradorPedidos.reportar(lp))
+    Await.result(fl, 10 seconds)
+    val elapsed = (System.nanoTime() - t1) / 1.0E09
+
+    println(s"tiempo de espera: estimado: $estimatedElapsed ,real: $elapsed")
     assert(elapsed >= estimatedElapsed)
   }
 }
